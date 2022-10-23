152301f0de777ca8b2566b5c0859873ce37e3f68
LAPTOP-EMTGJIHK\kang <2548066702@qq.com>
2022-10-23 16:28:12 +0800

    first

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+/*说程序员是面向搜索引擎编程，这是因为很多常见的错误，
+很多人都已经遇到过了，遇到bug先在网上搜索一下这是最高效的解决方法。但即使是在*/
+
+
+
+
+}
